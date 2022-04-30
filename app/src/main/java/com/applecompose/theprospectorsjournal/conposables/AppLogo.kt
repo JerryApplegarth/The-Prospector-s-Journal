1023ad096a08286d7e14b625a534d3a352475bd0
Jerry Applegarth <applegarth1630@gmail.com>
2022-04-30 10:39:54 -0700

    2.4 commit added app logo

--- app/src/main/java/com/applecompose/theprospectorsjournal/conposables/AppLogo.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/conposables/AppLogo.kt
@@ -1,8 +1,112 @@
 package com.applecompose.theprospectorsjournal.conposables
 
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.Image
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.applecompose.theprospectorsjournal.R
+import com.applecompose.theprospectorsjournal.ui.theme.mediumGreen
 
 @Composable
 fun AppLogo() {
 
+	Column(
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(16.dp),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		Spacer(modifier = Modifier.height(8.dp))
+		Image(
+			painter = painterResource(
+				id = R.drawable.four_apple_bmp
+			),
+			contentDescription = "An Apple for a logo",
+			Modifier.size(30.dp)
+		)
+		Text(
+			text = stringResource(R.string.apple_apps),
+			fontWeight = FontWeight.Bold,
+			color = MaterialTheme.colors.mediumGreen
+		)
+		Text(
+			text = stringResource(R.string.help_support_us),
+			modifier = Modifier.padding(4.dp)
+		)
+		Text(
+			text = stringResource(R.string.buy_me_coffee),
+			modifier = Modifier.padding(4.dp)
+		)
+
+
+		val annotatedLinkString: AnnotatedString = buildAnnotatedString {
+
+			val str = "click here  "
+			val startIndex = str.indexOf("click here")
+			val endIndex = startIndex + 10
+			append(str)
+			addStyle(
+				style = SpanStyle(
+					color = Color(0xff64B5F6),
+					fontSize = 18.sp,
+
+					textDecoration = TextDecoration.Underline
+				), start = startIndex, end = endIndex
+			)
+			addStringAnnotation(
+				tag = "URL",
+				annotation = "https://www.buymeacoffee.com/JerryApplegarth",
+				start = startIndex,
+				end = endIndex
+			)
+
+
+		}
+
+		val uriHandler = LocalUriHandler.current
+		ClickableText(
+			modifier = Modifier
+				.padding(start = 124.dp, end = 8.dp)
+				.fillMaxWidth()
+				.align(Alignment.CenterHorizontally),
+
+			text = annotatedLinkString,
+
+			onClick = {
+				annotatedLinkString
+					.getStringAnnotations("URL", it, it)
+					.firstOrNull()?.let { stringAnnotation ->
+						uriHandler.openUri(stringAnnotation.item)
+
+
+					}
+
+			})
+
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppLogoPreview() {
+	AppLogo()
 }
--- app/src/main/java/com/applecompose/theprospectorsjournal/ui/theme/Color.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/ui/theme/Color.kt
@@ -1,8 +1,58 @@
 package com.applecompose.theprospectorsjournal.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+//Light
+val Primary = Color(0xff96480c)
+val Secondary = Color(0xFF765847)
+val OnSurfaceVariant = Color(0xFF52443c)
+val OnSurface = Color(0xFF201a17)
+val OnPrimary = Color(0xFFffffff)
+val OnSecondary = Color(0xFFffffff)
+val PrimaryVariant = Color(0xFF5d6136)
+val NewBackground = Color(0xFFffdcb8)
+
+//Dark
+val DarkPrimary = Color(0xffffb688)
+val DarkSecondary = Color(0xFFe5bfaa)
+val DarkOnSurfaceVariant = Color(0xFFd7c2b8)
+val DarkOnSurface = Color(0xFFece0db)
+val DarkOnPrimary = Color(0xFF532200)
+val DarkOnSecondary = Color(0xFF432b1c)
+val DarkPrimaryVariant = Color(0xFFc6cb95)
+val DarkNewBackground = Color(0xFF2c1700)
+
+
+
+
+val Amber200 = Color(0xFFffe54c)
+val Amber500 = Color(0xFFffb300)
+val Amber700 = Color(0xFFc68400)
+val LightAmber = Color(0xFFffecb3)
+
+val MediumGreen = Color(0xFF3BAE06)
+
+
+
+val Tan = Color(0xFFDDBC83)
+
+val Colors.mediumGreen: Color
+    @Composable
+    get() = if (isLight) MediumGreen else MediumGreen
+
+
+val Colors.newBackgroundColor: Color
+    @Composable
+    get() = if (isLight) NewBackground else DarkNewBackground
+
+val Colors.fontColor: Color
+    @Composable
+    get() = if (isLight) DarkOnSecondary else DarkOnPrimary
