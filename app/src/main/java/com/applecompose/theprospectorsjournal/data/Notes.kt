90f06f920d5384ecf3ef44ca1152b7be6545c825
Jerry Applegarth <applegarth1630@gmail.com>
2022-05-01 17:31:13 -0700

    3.0 back to basic

--- app/src/main/java/com/applecompose/theprospectorsjournal/data/Notes.kt
+++
@@ -1,7 +0,0 @@
-package com.applecompose.theprospectorsjournal.data
-
-data class Notes(
-	val title: String = "",
-	val coordinates: String = "",
-	val notes: String = ""
-)
--- app/src/main/java/com/applecompose/theprospectorsjournal/screens/NoteScreen.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/screens/NoteScreen.kt
@@ -1,68 +1,36 @@
 package com.applecompose.theprospectorsjournal.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.applecompose.theprospectorsjournal.data.Notes
 
 @Composable
 fun NoteScreen(navController: NavController) {
-	val context = LocalContext.current
-	val titleValue = remember { mutableStateOf(TextFieldValue())}
-	val coordinatesValue = remember { mutableStateOf(TextFieldValue())}
-	val notesValue = remember { mutableStateOf(TextFieldValue())}
-	val notes = remember { mutableStateListOf(Notes())}
-	val isSaved = remember { mutableStateOf(false)}
-
-	NotesService.getNotes(notes)
-
 	Column(
-		modifier = Modifier
-			.fillMaxWidth()
-			.padding(8.dp)
-	) {
-		DetailInput(fieldValue = titleValue, label = "Title")
-		DetailInput(fieldValue = coordinatesValue, labal = "Coordinates", singleLine = false)
-		DetailInput(fieldValue = notesValue, label = "Notes", singleLine = false)
-		Spacer(modifier = Modifier.padding(4.dp))
-		Button(
-			onClick = {
-				val notes = Notes(
-					title = titleValue.value.text,
-					coordinates = coordinatesValue.value.text,
-					notes = notesValue.value.text
+				modifier = Modifier
+					.fillMaxSize(),
+				horizontalAlignment = Alignment.CenterHorizontally,
+				verticalArrangement = Arrangement.Top
 				)
-				db.collection("notes")
-					.add(notes)
-			}) {
 
+			{
 
-		}
+				Text(
+					text = "Gold Prices",
+					fontSize = 24.sp,
+					)
+				Spacer(modifier = Modifier.height(16.dp))
+				Divider(thickness = 2.dp, color = MaterialTheme.colors.primaryVariant)
+				Button(
+					onClick = {  }) {
+					Text(text = "Save")
 
-	}
+				}
 
-
-}
-
-
-fun <T> SnapshotStateList<T>.updateList(newList: List<T>){
-	clear()
-	addAll(newList)
-}
-
-@Composable
-fun DetailInput(
-	fieldValue: MutableState<TextFieldValue>,
-) {
-
-
+			}
 }
