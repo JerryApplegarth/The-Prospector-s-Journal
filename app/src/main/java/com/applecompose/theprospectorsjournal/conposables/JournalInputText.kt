9c926c352fa4fa7831da612f300752c91ec00cf3
Jerry Applegarth <applegarth1630@gmail.com>
2022-05-07 13:51:46 -0700

    4.5 commit Journal Input Text completed

--- app/src/main/java/com/applecompose/theprospectorsjournal/conposables/JournalInputText.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/conposables/JournalInputText.kt
@@ -1,9 +1,48 @@
 package com.applecompose.theprospectorsjournal.conposables
 
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import com.applecompose.theprospectorsjournal.data.JournalData
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import com.applecompose.theprospectorsjournal.ui.theme.newBackgroundColor
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun JournalInputText() {
+fun JournalInputText(
+	modifier: Modifier = Modifier,
+	text: String,
+	label: String,
+	maxLines: Int = 1,
+	onTextChange: (String) -> Unit,
+	onImeAction: () -> Unit = {}
+) {
+	val keyboardController = LocalSoftwareKeyboardController.current
+	OutlinedTextField(
+		value = text,
+		onValueChange = onTextChange,
+		colors = TextFieldDefaults.textFieldColors(
+			backgroundColor = MaterialTheme.colors.newBackgroundColor
+		),
+		maxLines = maxLines,
+		label = {
+			Text(text = label)
+
+		},
+		keyboardOptions = KeyboardOptions.Default.copy(
+			imeAction = ImeAction.Done
+		),
+		keyboardActions = KeyboardActions(onDone = {
+			onImeAction()
+			keyboardController?.hide()
+		}),
+		modifier = modifier
+		)
 
 }
