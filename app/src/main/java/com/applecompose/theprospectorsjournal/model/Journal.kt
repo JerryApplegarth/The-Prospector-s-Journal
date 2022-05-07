c0290f27b630b3350bd0823cd4a58e5d3f591f13
Jerry Applegarth <applegarth1630@gmail.com>
2022-05-07 16:24:39 -0700

    4.7 commit entity-journal

--- app/src/main/java/com/applecompose/theprospectorsjournal/model/Journal.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/model/Journal.kt
@@ -1,4 +1,29 @@
 package com.applecompose.theprospectorsjournal.model
 
-class Journal {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
+import java.util.*
+
+@Entity(tableName = "journal_table")
+data class Journal(
+	@PrimaryKey
+	val id: UUID = UUID.randomUUID(),
+
+	@ColumnInfo(name = "prospect_name")
+	val title: String,
+
+	@ColumnInfo(name = "location")
+	val location: String,
+
+	@ColumnInfo(name = "ownership")
+	val ownership: String,
+
+	@ColumnInfo(name = "comments")
+	val comments: String,
+
+	@ColumnInfo(name = "journal_entry_date")
+	val entryDate: Date = Date.from(Instant.now())
+
+)
