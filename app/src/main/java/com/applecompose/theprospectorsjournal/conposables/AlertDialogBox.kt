43cfc8f6fec84ad2b86d947622f0097449ff4233
Jerry Applegarth <applegarth1630@gmail.com>
2022-05-07 12:44:47 -0700

    4.3 commit Alert Box Completed

--- app/src/main/java/com/applecompose/theprospectorsjournal/conposables/AlertDialogBox.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/conposables/AlertDialogBox.kt
@@ -1,8 +1,70 @@
 package com.applecompose.theprospectorsjournal.conposables
 
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.applecompose.theprospectorsjournal.R
 
 @Composable
 fun AlertDialogBox() {
+	MaterialTheme {
+		Column {
+			val openDialog = remember { mutableStateOf(false)  }
 
+			Button(onClick = {
+				openDialog.value = true
+			}) {
+				Text(stringResource(id = R.string.delete_note))
+			}
+
+			if (openDialog.value) {
+
+				AlertDialog(
+					onDismissRequest = {
+
+						openDialog.value = false
+					},
+					title = {
+						Text(text = stringResource(id = R.string.alert_box))
+					},
+					text = {
+						Text(stringResource(id = R.string.want_to_delete))
+					},
+					confirmButton = {
+						Button(
+
+							onClick = {
+								openDialog.value = false
+							}) {
+							Text("Confirm")
+						}
+					},
+					dismissButton = {
+						Button(
+
+							onClick = {
+								openDialog.value = false
+							}) {
+							Text("Dismiss")
+						}
+					}
+				)
+			}
+		}
+
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AlertDialogBoxPreview() {
+	AlertDialogBox()
 }
