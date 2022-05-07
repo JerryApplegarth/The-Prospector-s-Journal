fedc4684c3468edc1ae66858f4a22f3602a61aea
Jerry Applegarth <applegarth1630@gmail.com>
2022-05-07 09:22:03 -0700

    4.0 commit picture added to the gold price page

--- app/src/main/java/com/applecompose/theprospectorsjournal/screens/GoldPrices.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/screens/GoldPrices.kt
@@ -1,40 +1,43 @@
 package com.applecompose.theprospectorsjournal.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.applecompose.theprospectorsjournal.navigation.Screen
+import com.applecompose.theprospectorsjournal.R
+import com.applecompose.theprospectorsjournal.conposables.ImageCard
 
 @Composable
 fun GoldPrices(navController: NavController) {
 	Column(
 		modifier = Modifier
-			.fillMaxWidth(),
+			.fillMaxWidth()
+			.padding(6.dp),
 		horizontalAlignment = Alignment.CenterHorizontally,
 		verticalArrangement = Arrangement.Top
-	)
+	) {
+		val painter = painterResource(id = R.drawable.nugget)
+		val description = "picture of large gold nugget"
+		val title = "Large nugget from the 4 Apple Claim in Southern Oregon"
 
-	{
-
-		Text(
-			text = "Gold Prices",
-			fontSize = 24.sp,
+		ImageCard(
+			painter = painter,
+			contentDescription = description,
+			title = title
 		)
-		Spacer(modifier = Modifier.height(16.dp))
-		Divider(thickness = 2.dp, color = MaterialTheme.colors.primaryVariant)
-		Button(
-			onClick = {
-				navController.navigate(route = Screen.HomeScreen.route)
-			}) {
-			Text(text = "Home")
-
+		Column(
+			modifier = Modifier
+				.padding(6.dp)
+		) {
+			Spacer(modifier = Modifier.height(12.dp))
+			Divider(thickness = 2.dp, color = Color.Black)
+			Spacer(modifier = Modifier.height(12.dp))
 		}
 	}
-
 
 }
