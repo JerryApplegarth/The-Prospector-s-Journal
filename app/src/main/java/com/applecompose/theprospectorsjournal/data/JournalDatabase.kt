2afd35d778681faab340efb2ec37bb9208de79ce
Jerry Applegarth <applegarth1630@gmail.com>
2022-05-07 18:38:49 -0700

    5.0 commit journal database and journal Database dao

--- app/src/main/java/com/applecompose/theprospectorsjournal/data/JournalDatabase.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/data/JournalDatabase.kt
@@ -1,4 +1,15 @@
 package com.applecompose.theprospectorsjournal.data
 
-class JournalDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.applecompose.theprospectorsjournal.model.Journal
+import com.applecompose.theprospectorsjournal.utils.DataConverter
+import com.applecompose.theprospectorsjournal.utils.UUIDConverter
+
+@Database(entities = [Journal::class], version = 1, exportSchema = false)
+@TypeConverters(DataConverter::class, UUIDConverter::class)
+abstract class JournalDatabase: RoomDatabase() {
+	abstract fun journalDao(): JournalDatabaseDao
+
 }
--- app/src/main/java/com/applecompose/theprospectorsjournal/data/JournalDatabaseDao.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/data/JournalDatabaseDao.kt
@@ -1,8 +1,28 @@
 package com.applecompose.theprospectorsjournal.data
 
 import androidx.compose.runtime.Composable
+import androidx.room.*
+import com.applecompose.theprospectorsjournal.model.Journal
+import kotlinx.coroutines.flow.Flow
 
-@Composable
-fun JournalDatabaseDao() {
+@Dao
+interface JournalDatabaseDao {
 
+	@Query("SELECT * FROM journal_table")
+	fun getJournals(): Flow<List<Journal>>
+
+	@Query("SELECT * FROM journal_table WHERE id = :id")
+	suspend fun getJournalById(id: String): Journal
+
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insert(journal: Journal)
+
+	@Update(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun update(journal: Journal)
+
+	@Query("DELETE FROM journal_table")
+	suspend fun deleteAll()
+
+	@Delete
+	suspend fun deleteNote(journal: Journal)
 }
