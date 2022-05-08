0d10a95463be5bba85e3846c4c2cc19841b2a75d
Jerry Applegarth <applegarth1630@gmail.com>
2022-05-08 07:04:50 -0700

    5.2 commit repository

--- app/src/main/java/com/applecompose/theprospectorsjournal/repository/JournalRepository.kt
+++ app/src/main/java/com/applecompose/theprospectorsjournal/repository/JournalRepository.kt
@@ -1,4 +1,22 @@
 package com.applecompose.theprospectorsjournal.repository
 
-class JournalRepository {
+import android.provider.ContactsContract
+import com.applecompose.theprospectorsjournal.data.JournalDatabaseDao
+import com.applecompose.theprospectorsjournal.model.Journal
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class JournalRepository @Inject constructor(
+	private val journalDatabaseDao: JournalDatabaseDao
+) {
+	suspend fun addJournal(journal: Journal) = journalDatabaseDao.insert(journal)
+	suspend fun updateJournal(journal: Journal) = journalDatabaseDao.update(journal)
+	suspend fun deleteJournal(journal: Journal) = journalDatabaseDao.deleteNote(journal)
+	suspend fun deleteAllJournal() = journalDatabaseDao.deleteAll()
+
+	fun getAllJournals(): Flow<List<Journal>> =
+		journalDatabaseDao.getJournals().flowOn(Dispatchers.IO).conflate()
 }
